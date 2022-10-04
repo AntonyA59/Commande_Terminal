f7f9efdda27e66af9c27b9b52e992b7afabb40eb
Antony <antony.alsberghe@hotmail.fr>
2022-10-04 13:57:25 +0200

    LinuxCommand added

---
+++ LinuxCommand.bash
@@ -0,0 +1,14 @@
+# Télécharger les mis à jours
+apt update
+# Installer la mis à jours
+apt upgrade
+
+# Installe un logiciel
+apt install <logiciel>
+
+# Exécute un fichier
+cat <file>
+
+##Options Linux
+# Pour accepter automatiquement les questions.
+-y
